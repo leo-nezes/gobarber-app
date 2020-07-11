162f69ae97d44d93a74444f6d86b82473ad0315a
Leonardo Menezes <leonhard.menezes@gmail.com>
2020-07-11 14:48:38 -0300

    [Bootcamp GoStack] Dashboard - Adicionando authorization ao header da requisição - Buascando dados da API e adicionando no FlatList

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -45,8 +45,11 @@ const AuthProvider: React.FC = ({ children }) => {
         '@GoBarber:user',
       ]);
 
-      if (token[1] && user[1])
+      if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
+      }
 
       setLoading(false);
     }
@@ -66,6 +69,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
