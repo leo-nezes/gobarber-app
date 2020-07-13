434774b4fc951ac29a275763f9a770019c61087f
Leonardo Menezes <leonhard.menezes@gmail.com>
2020-07-13 15:02:39 -0300

    [Bootcamp GoStack] Agendamento - Criando e estilizando página CreateAppointment - Adicionando padding-top na Dashboard

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { FlatList } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { Provider } from './index';
 
 export const Container = styled.View`
@@ -9,6 +10,7 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   padding: 24px;
+  padding-top: ${getStatusBarHeight() + 24}px;
   background: #28262e;
 
   flex-direction: row;
